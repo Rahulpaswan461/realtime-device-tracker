89da0887c7eb6904566637d03852a40be6f5d962
Rahul Paswan <rahulpaswandoon@gmail.com>
2024-08-02 21:08:10 +0530

    save changes

--- public/js/script.js
+++ public/js/script.js
@@ -15,7 +15,7 @@ if(navigator.geolocation){
 
 const map = L.map("map").setView([0,0],16)
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution:"rahul"
+    attribution:"Current Location"
 }).addTo(map)
 
 let markers = {}
